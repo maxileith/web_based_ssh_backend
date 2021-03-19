81c68812de32be66de0b0493ed0181d72254c452
Tobias <derrumpli+github@posteo.de>
2021-03-19 09:48:13 +0100

    Initializes django channels

--- web_based_ssh_backend/asgi.py
+++ web_based_ssh_backend/asgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_based_ssh_backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+
+})
+# application = get_asgi_application()
