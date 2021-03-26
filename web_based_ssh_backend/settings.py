ea1918efeaf3b7d66daf29d5458a5d688752b0f5
Maximilian Leith <maximilian@leith.de>
2021-03-26 10:54:12 +0100

    redis settings

--- web_based_ssh_backend/settings.py
+++ web_based_ssh_backend/settings.py
@@ -123,8 +123,11 @@ STATIC_URL = '/static/'
 
 ASGI_APPLICATION = "web_based_ssh_backend.asgi.application"
 
-# CHANNEL_LAYERS = {
-#    "default": {
-#        "BACKEND": "channels.layers.InMemoryChannelLayer"
-#    }
-# }
+CHANNEL_LAYERS = {
+    "default": {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    }
+}
